e4e73519cfe17b7138488d283953fc3df6498766
Administrator <Administrator@JUSTMINE-PC>
2017-08-03 14:27:41 +0800

    添加项目文件。

---
+++ HangFire.Samples/Controllers/HomeController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Hangfire;
+
+namespace HangFire.Samples.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            ViewData["Message"] = "Your application description page.";
+            BackgroundJob.Enqueue(() => Console.WriteLine("Created a new fire-and-forget job"));
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            ViewData["Message"] = "Your contact page.";
+
+            return View();
+        }
+
+        public IActionResult Error()
+        {
+            return View();
+        }
+    }
+}
